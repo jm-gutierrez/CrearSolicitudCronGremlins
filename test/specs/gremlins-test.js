1d063b8f9e8402342d19f40efa0d6d084ed9f002
Johanna Gutiérrez Meza <31225028+jm-gutierrez@users.noreply.github.com>
2019-09-08 22:47:07 -0500

    Update gremlins-test.js

--- test/specs/gremlins-test.js
+++ test/specs/gremlins-test.js
@@ -14,8 +14,26 @@ function unleashGremlins(ttl, callback) {
     horde.stop();
     callback();
   }
-  var horde = window.gremlins.createHorde();
+  var horde = window.gremlins.createHorde().gremlin(
+    gremlins.species.clicker().clickTypes(['click']).canClick(
+        function(element){
+          //return false if the element is neither a link nor a button
+          return (element.type == "a" || element.type === "button");
+        }
+      )
+    ).gremlin(
+      gremlins.species.formFiller().canFillElement(
+        function(element){
+          //return false if the element is disabled or is an input which can not be filled with characteres
+          return !element.hasAttribute("disabled") && (element.getAttribute('type') != 'button' && element.getAttribute('type') != 'checkbox' && element.getAttribute('type') != 'radio' && element.getAttribute('type') != 'submit');
+        }
+      )
+    ).gremlin(gremlins.species.toucher())
+      .gremlin(gremlins.species.scroller());
+
   horde.seed(1234);
+  //Asing priorities to: click => 0.7, fillForm => 0.1, toucher => 0.1 and scroller => 0.1 
+  horde.strategy(gremlins.strategies.distribution().distribution([0.7, 0.1, 0.1, 0.1]));
 
   horde.after(callback);
   window.onbeforeunload = stop;
